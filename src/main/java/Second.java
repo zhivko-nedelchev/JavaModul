adb3eaac60e674348cd984a37821c857e283c9a7
zhivko-nedelchev <bushi1972@yahoo.dk>
2024-02-07 21:29:23 +0200

    Initial commit

---
+++ src/main/java/Second.java
@@ -0,0 +1,5 @@
+public class Second {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.crypto.spec.IvParameterSpec;
+
+public class Main {
+    public static void main(String[] args) {
+        /*String firstName = "Galina";
+        String surname = "Iv";
+        String lastName = "Tileva";
+        System.out.println(firstName + " " + surname + " " + lastName);
+
+        String age = "26";
+        System.out.println(age);
+        String email = "galina_tileva@abv.bg";
+        System.out.println(email);
+        String address = "London, Whittington court, Aylmer road 16";
+        System.out.println(address);
+        String number = "07438091383";
+        System.out.println(number);
+        String postCode = "N20BT";
+        System.out.println(postCode);
+*/
+
+    }
+    calculateAVG( numberOne: 2, numberTwo:6);
+    public static void calculateAVG(int numberOne, int numberTwo) {
+        int result = (numberOne + numberTwo)/2;
+        System.out.println(result);
+    }
+
+}
+
